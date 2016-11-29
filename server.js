009d97890fce486076409e65c715f76bb247134e
Srijith Sasidharan <srijith.sasidharan@cognizant.com>
2016-11-29 14:52:46 +0530

    modified above files

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Get secrets from server environment
-var botConnectorOptions = { 
-    appId: process.env.BOTFRAMEWORK_APPID || '26b5fb26-c388-4da4-b28d-9c5c64087e08', 
-    appSecret: process.env.BOTFRAMEWORK_APPSECRET || 'ekomS320SrzwRXvTnqbyw31'
-};
-
-// Create bot
-var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID || 'f9f917b6-bf62-413d-9cf3-9e9ee84d37f1',
+    appPassword: process.env.MICROSOFT_APP_PASSWORD || 'V46X32ymeXx70mDP6edbZvn'
+});
 
-// Handle Bot Framework messages
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+/*var connector = new builder.ChatConnector({
+    appId: null ,
+    appPassword: null 
+});*/
 
-// Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
 });
